2f1c46fa58fcec4351c01ff1168f67ccea85c6a2
eulbyvan <work.issdq@gmail.com>
2022-12-05 10:13:44 +0700

    added more error status

--- src/main/java/com/enigmacamp/WalletController.java
+++ src/main/java/com/enigmacamp/WalletController.java
@@ -32,14 +32,16 @@ public class WalletController {
 
 		ErrorResponse res = new ErrorResponse();
 
-		res.setCode("X01");
 		res.setStatus("FAILED");
 
 		if (!req.getCustomerId().equals("123")) {
+			res.setCode("X01");
 			res.setMsg("Customer ID is unknown");
 		} else if (!req.getTrxId().equals("123")) {
+			res.setCode("X02");
 			res.setMsg("Transaction ID is unknown");
 		} else if (!req.getCustomerId().equals("PAYMENT")) {
+			res.setCode("X03");
 			res.setMsg("Transaction type is unknown");
 		}
 
